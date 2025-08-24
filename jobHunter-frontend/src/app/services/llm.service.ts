9090152807e6c0e24f7e6ad5e47d5928721705b8
Gabriel Ardevol <artsdevol@gmail.com>
2025-08-25 01:49:13 +0200

    llm service scaffold

---
+++ jobHunter-frontend/src/app/services/llm.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, of } from 'rxjs';
+import { Offer } from '../models/models';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LlmService {
+    llmUrl: string = "https://-------------------"
+
+    constructor() {
+    }
+    
+    promptOffer() {
+    }
+    
+    promptResponse() {
+    }
+    
+}
