d87f1b9c5dddbd656e5e6fd731831ac7ae6194ae
Gabriel Ardevol <artsdevol@gmail.com>
2025-08-25 00:23:39 +0200

    offer.service -> added mock conditional

--- jobHunter-frontend/src/app/services/offers.service.ts
+++ jobHunter-frontend/src/app/services/offers.service.ts
@@ -9,6 +9,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class OffersService {
 
+  mock: boolean = true;
   private mockOffers: Offer[] = [...OFFERS];
   private offersSubject: BehaviorSubject<Offer[]> = new BehaviorSubject<Offer[]>([...OFFERS]);
   offers$: Observable<Offer[]> = this.offersSubject.asObservable();
@@ -17,6 +18,9 @@ export class OffersService {
   }
 
   fetchOffers() {
+    // si mock = true
+    //    agafa les ofertes mock i les desa com a offerSubject
+    // si mock = false ---->
     // si està autenticat, crida a backend
     // si no, crida indexedDB
     // desa totes les ofertes com a offersSubject
@@ -25,18 +29,21 @@ export class OffersService {
   addOffer(offer: Offer) {
     const currentOffers = this.offersSubject.value;
     this.offersSubject.next([...currentOffers, offer]);
+    // si mock = false ------------
     // si està autenticat, desa l'oferta a backend
     // si no, la desa a indexedDB
   }
 
   updateOffer(index: number, offer: Offer) {
     //actualitza la oferta al behaviourSubject
+    // si mock = false ------------
     //si està autenticat, actualitza la oferta al backend
     // si no, actualitza la oferta a indexedDB
   }
 
   deleteOffer(index: number) {
     //actualitza la oferta al behaviourSubject
+    // si mock = false ------------
     //si està autenticat, actualitza la oferta al backend
     // si no, actualitza la oferta a indexedDB
   }
