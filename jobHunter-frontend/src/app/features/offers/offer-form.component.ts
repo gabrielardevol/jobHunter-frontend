b309f7a04db0917957df5848d6aafa650db37bb7
Gabriel Ardevol <artsdevol@gmail.com>
2025-08-26 23:45:31 +0200

    refactor offer-form.component

--- jobHunter-frontend/src/app/features/offers/offer-form.component.ts
+++ jobHunter-frontend/src/app/features/offers/offer-form.component.ts
@@ -1,8 +1,8 @@
-import { Component, DestroyRef, EventEmitter, Output } from '@angular/core';
+import { Component, DestroyRef } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { OffersService } from '../../services/offers.service';
-import { debounceTime, Observable, of, switchMap, take, tap } from 'rxjs';
+import { debounceTime, filter, Observable, switchMap, tap } from 'rxjs';
 import { LlmService } from '../../services/llm.service';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { CommonModule } from '@angular/common';
@@ -14,42 +14,79 @@ import { ModalService } from '../../services/modals.service';
   selector: 'app-offer-form',
   imports: [ReactiveFormsModule, CommonModule],
   template: `
-    <input [formControl]="llmControl" placeholder="Type something..." />
+
+    <input [formControl]="llmControl" placeholder="copy-paste offer description"/>
+    <span *ngIf="isLoading">Processing...</span>
 
     <form [formGroup]="offerForm" (ngSubmit)="onSubmit()"> 
     
-      <input formControlName="company" placeholder="Company" />
-    
-      <select formControlName="role">
-        <option value="frontend">frontend</option>
-        <option value="backend">backend</option>
-        <option value="fullstack">fullstack</option>
-        <option value="others">others</option>
-      </select>
-      
-      <input formControlName="location" placeholder="Location" />
-      <input formControlName="recruiter" placeholder="Recruiter" />
-      <input formControlName="skills" placeholder="Skills" />
-      <input formControlName="platform" placeholder="Platform" />
-      <select formControlName="paymentType">
-        <option value="month">month</option>
-        <option value="hour">hour</option>
-        <option value="year">year</option>
-        <option value="day">day</option>
-      </select>
-      <input formControlName="salaryMinimum" type="number" placeholder="Min per hour" />
-      <input formControlName="salaryMaximum" type="number" placeholder="Max per hour" />
-      <input formControlName="weeklyHours" type="number" placeholder="Weekly hours" />
-      <input formControlName="duration" type="number" placeholder="Duration in months" />
-      <input formControlName="experienceMinimum" type="number" placeholder="Min experience" />
-      <input formControlName="experienceMaximum" type="number" placeholder="Max experience" />
+      <label>
+        Company <input formControlName="company" placeholder="Company" aria-required="true" />
+      </label>
 
-      <button type="button" (click)="onClose()">Cancel</button>
-      <button [disabled]="!offerForm.valid" type="submit">
-        Create Offer
-      </button>
+      <label>
+        Role
+        <select formControlName="role">
+          <option value="frontend">frontend</option>
+          <option value="backend">backend</option>
+          <option value="fullstack">fullstack</option>
+          <option value="others">others</option>
+        </select>
+      </label>
+
+      <label>
+        Location <input formControlName="location" placeholder="Location" />
+      </label>
+
+      <label>
+        Recruiter <input formControlName="recruiter" placeholder="Recruiter" />
+      </label>
+
+      <label>
+        Skills <input formControlName="skills" placeholder="Skills" />
+      </label>
+
+      <label>
+        Platform <input formControlName="platform" placeholder="Platform" />
+      </label>
+
+      <label>
+        Payment Type <select formControlName="paymentType">
+          <option value="month">month</option>
+          <option value="hour">hour</option>
+          <option value="year">year</option>
+          <option value="day">day</option>
+        </select>
+      </label>
+
+      <label>
+        Minimum Salary <input formControlName="salaryMinimum" type="number" placeholder="Min per hour" />
+      </label>
+
+      <label>
+        Maximum Salary <input formControlName="salaryMaximum" type="number" placeholder="Max per hour" />
+      </label>
+
+      <label>
+        Weekly Hours <input formControlName="weeklyHours" type="number" placeholder="Weekly hours" />
+      </label>
+
+      <label>
+        Duration (months) <input formControlName="duration" type="number" placeholder="Duration in months" />
+      </label>
+
+      <label>
+        Minimum Experience <input formControlName="experienceMinimum" type="number" placeholder="Min experience" />
+      </label>
+
+      <label>
+        Maximum Experience <input formControlName="experienceMaximum" type="number" placeholder="Max experience" />
+      </label>
+
+      <button type="button" (click)="modalService.close()">Cancel</button>
+      <button [disabled]="!offerForm.valid" type="submit">Create Offer</button>
+      
     </form>
-    <hr>
   `,
 
   styles: ``
@@ -57,9 +94,7 @@ import { ModalService } from '../../services/modals.service';
 export class OfferFormComponent {
   offerForm: FormGroup;
   llmControl: FormControl = new FormControl;
-  textSource$: Observable<string> = this.llmControl.valueChanges.pipe(
-    debounceTime(700),
-  );
+  isLoading: boolean = false;
   
   constructor(
     private fb: FormBuilder, 
@@ -67,10 +102,39 @@ export class OfferFormComponent {
     public offersService: OffersService, 
     public llmService: LlmService, 
     private destroyRef: DestroyRef,
-    private modalService: ModalService
+    public modalService: ModalService
   ) {
-    this.offerForm = this.fb.group({
-      id: uuidv4(),
+    this.offerForm = this.buildForm();
+  }
+
+  ngOnInit(): void {
+    this.llmControl.valueChanges.pipe(
+      debounceTime(500),
+      filter(text => text && text.trim().length > 0),
+      tap(() => this.isLoading = true),
+      switchMap(text => this.llmService.promptOffer(text)),
+      takeUntilDestroyed(this.destroyRef),
+      tap(parsed => { 
+        this.offerForm.patchValue(parsed);
+        this.isLoading = false;
+      })
+    ).subscribe();
+  }
+
+  onSubmit() {
+    const offer = { ...this.offerForm.value, id: uuidv4() };
+
+    this.offersService.addOffer(offer);
+    this.textSourceService.addTextSource({
+      content: this.llmControl.value, 
+      offerId: offer.id 
+    });
+    this.llmControl.reset();
+    this.offerForm.reset()
+  }
+
+  buildForm() {
+    return this.fb.group({
       company: ['', Validators.required],
       role: ['', Validators.required],
       location: [''],
@@ -81,32 +145,9 @@ export class OfferFormComponent {
       salaryMinimum: [0, Validators.min(0)],
       salaryMaximum: [0, Validators.min(0)],
       weeklyHours: [0, Validators.min(0)],
-      duration: [undefined],
+      duration: [null],
       experienceMinimum: [0, Validators.min(0)],
       experienceMaximum: [0, Validators.min(0)],
-    });
-  }
-
-  ngOnInit(): void {
-    this.textSource$
-    .pipe(
-      switchMap(text => this.llmService.promptOffer(text)),
-      takeUntilDestroyed(this.destroyRef)
-    )
-    .subscribe(parsed => {
-      this.offerForm.patchValue(parsed);
-    });
-  }
-
-  onSubmit() {
-    this.offersService.addOffer(this.offerForm.value);
-    this.textSourceService.addTextSource({
-      content: this.llmControl.value, 
-      offerId: this.offerForm.value.id });
-    this.llmControl.reset();
-  }
-
-  onClose(){
-    this.modalService.close()
+    })
   }
 }
